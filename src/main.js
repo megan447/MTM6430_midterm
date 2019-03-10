7c35fbfc95b264846810dd31fff10bb4f7ccb5e3
Lu Xie <xiemegan447@gmail.com>
2019-03-10 17:26:35 -0400

    unfinished

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
+// cores and libs
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.component.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueForm from 'vue-form'
+
+// internal libs
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+Vue.use(VueForm)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
