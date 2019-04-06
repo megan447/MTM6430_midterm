6fbeb80a2eaba80d94a128a43e1f9bb1d6082b8c
Fangyun,Liu <liu00433@algonquinlive.com>
2019-04-05 22:12:27 -0400

    finished the shared folder

---
+++ src/components/shared/map-view/map-view.component.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="mapView">
+        <div class="map-view-wrapper">
+           <div class="address-list-wrapper" v-if="showList">
+                 Some List
+           </div>
+           <div class="map-wrapper">
+              <GmapMap
+                :center="{lat:10, lng:10}"
+                :zoom="7"
+                style="width: 100%; height: 300px"
+                >
+                <GmapMarker
+                    :key="index"
+                    v-for="(m, index) in markers"
+                    :position="m.position"
+                    @click="center=m.position"
+                />
+               </GmapMap>
+           </div>
+        </div>
+    </div>
+</template>
+<script src="./map-view.component.js"></script>
+<style src="./map-view.component.css" scoped></style>
